6096a641a0708d8b775903a277a62df336c41ffd
Donna27 <percussion27@hotmail.com>
2017-02-20 11:22:30 +0700

    Add Reminder components

--- app/Http/Controllers/ReminderController.php
+++ app/Http/Controllers/ReminderController.php
@@ -3,11 +3,21 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
+use App\Model\Reminder;
 class ReminderController extends Controller
 {
     public function home(){
-
         return view('home');
+    }
+
+    public function addReminder(Request $request){
+        $reminder = new Reminder();
+        $reminder->body = $request->reminder;
+        $reminder->finished = false;
+        $reminder->createdUserID = Auth::id();
+        $reminder->save();
+        return back();
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,4 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'ReminderController@home');
+Route::post('/reminder/new','ReminderController@addReminder');
